b9e5668bce01ce145c5595f0daf9c9226b0cfbec
Tripat-Jot <triptjot3@gmail.com>
2021-05-03 11:22:45 +0530

    Add changes

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,23 +1,21 @@
 const request = require('request')
 
-
-const geocode = (address , callback ) => {
+const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoidHJpcHRqb3QiLCJhIjoiY2tteWRlYzljMDJ2ZzJ3bWZ2NmQwdHdmYSJ9.UQKYJIEQqpoA_zvCy8ZIqw&limit=1'
 
-    request ({url , json:true} , (error , {body})=> {
-        if (error ) {
-            callback('Unable to connect to location services !!!' , undefined)
-        }
-        else if (body.features.lenght === 0) {
-            callback('Unable to find location !!!' , undefined)
-        }
-        else {
-            callback(undefined , {
-                latitude: body.features[0].center[0] ,
-                longitiude: body.features[0].center[1] ,
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
                 location: body.features[0].place_name
             })
         }
     })
 }
-module.exports = geocode 
+
+module.exports = geocode
